ee9feaf71040b343c2c8699a920523e975206590
nifi <nifi>
2006-07-07 06:45:45 +0000

    changed to use constant instead of numeric value

--- core/net/tcpip.c
+++ core/net/tcpip.c
@@ -82,7 +82,7 @@ packet_input(void)
 {
   if(uip_len > 0) {
     if(forwarding) {
-      if(uip_fw_forward() == 0) {
+      if(uip_fw_forward() == UIP_FW_LOCAL) {
 	uip_input();
 	if(uip_len > 0) {
 #if UIP_CONF_TCP_SPLIT
