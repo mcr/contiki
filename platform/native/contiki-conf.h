0da9c38f815e81a72c925a0391dcbd6631ce0185
oliverschmidt <oliverschmidt>
2006-08-13 15:13:21 +0000

    Added defines for declaring functions/data which are part of the CCI (Contiki Core Interface) and CLI (Contiki Loadable Interface). Both defines are empty for all platforms but the upcoming Win32 port.

--- platform/native/contiki-conf.h
+++ platform/native/contiki-conf.h
@@ -42,6 +42,9 @@
 #define CC_CONF_VA_ARGS                1
 #define CC_CONF_INLINE                 inline
 
+#define CCI
+#define CLI
+
 typedef uint8_t u8_t;
 typedef uint16_t u16_t;
 typedef uint32_t u32_t;
