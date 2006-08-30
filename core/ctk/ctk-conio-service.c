f82cdad0962886b763abdf1a32141c212e315c46
oliverschmidt <oliverschmidt>
2006-08-30 22:15:25 +0000

    Don't make assumptions on textentry contents beyond the first '\0'.

--- core/ctk/ctk-conio-service.c
+++ core/ctk/ctk-conio-service.c
@@ -163,8 +163,11 @@ draw_widget(struct ctk_widget *w,
 	   w->widget.textentry.ypos == j) {
 	  revers(0);
 	  cputcxy(xpos, ypos, '>');
+	  c = 1;
 	  for(i = 0; i < w->w; ++i) {
-	    c = text[i + xscroll];
+	    if(c != 0) {
+	      c = text[i + xscroll];
+	    }
             revers(i == w->widget.textentry.xpos - xscroll);
 	    if(c == 0) {
 	      cputc(' ');
