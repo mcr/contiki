997de163294d8006e564e39d5a328beff4d30850
nifi <nifi>
2006-10-09 21:05:37 +0000

    added active flags because some sensors react on radio activity

--- platform/esb/dev/tr1001.h
+++ platform/esb/dev/tr1001.h
@@ -149,6 +149,8 @@ unsigned short tr1001_packets_ok(void);
 unsigned short tr1001_packets_dropped(void);
 void tr1001_clear_packets(void);
 unsigned short tr1001_sstrength_value(unsigned int type);
+unsigned char tr1001_active(void);
+void tr1001_clear_active(void);
 #endif /* TR1001_STATISTICS */
 
 #endif /* __TR1001_H__ */
