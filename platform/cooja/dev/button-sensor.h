f3952cfad70bb59866a346a228f5abec947af99d
adamdunkels <adamdunkels>
2007-03-20 20:09:17 +0000

    Added lib/sensors.h include

--- platform/cooja/dev/button-sensor.h
+++ platform/cooja/dev/button-sensor.h
@@ -29,11 +29,13 @@
  * $Id$
  */
 
-#ifndef __BUTTON_H__
-#define __BUTTON_H__
+#ifndef __BUTTON_SENSOR_H__
+#define __BUTTON_SENSOR_H__
+
+#include "lib/sensors.h"
 
 extern const struct sensors_sensor button_sensor;
 
 #define BUTTON_SENSOR "Button"
 
-#endif /* __BUTTON_H__ */
+#endif /* __BUTTON_SENSOR_H__ */
