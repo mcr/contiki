6c2fe275f75384be343f6cc3b183823de8cca0b4
oliverschmidt <oliverschmidt>
2007-03-26 23:01:11 +0000

    Allow to build telnet-server example for targets not defining CTK_CONF_MAXMENUITEMS.

--- core/ctk/ctk.c
+++ core/ctk/ctk.c
@@ -774,7 +774,7 @@ ctk_menuitem_add(CC_REGISTER_ARG struct ctk_menu *menu,
 		 char *name)
 {
 #if CTK_CONF_MENUS
-  if(menu->nitems == CTK_CONF_MAXMENUITEMS) {
+  if(menu->nitems == CTK_MAXMENUITEMS) {
     return 0;
   }
   menu->items[menu->nitems].title = name;
