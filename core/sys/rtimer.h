5525d9f0832def13d2bb51da9734326fe8c51535
adamdunkels <adamdunkels>
2007-03-28 20:28:22 +0000

    Fixed API to be consistent between .h and .c file

--- core/sys/rtimer.h
+++ core/sys/rtimer.h
@@ -133,6 +133,8 @@ void rtimer_arch_init(void);
 void rtimer_arch_schedule(rtimer_clock_t t);
 rtimer_clock_t rtimer_arch_now(void);
 
+#define RTIMER_SECOND RTIMER_ARCH_SECOND
+
 #include "rtimer-arch.h"
 
 #endif /* __RTIMER_H__ */
