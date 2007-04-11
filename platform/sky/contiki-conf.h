9b37fba55f97e412737e554a8612106fe4935000
bg- <bg->
2007-04-11 15:22:05 +0000

    * Bump maximum number of udp sockets to 16.
    * Remove unused stuff.

--- platform/sky/contiki-conf.h
+++ platform/sky/contiki-conf.h
@@ -16,8 +16,6 @@
 #define CCIF
 #define CLIF
 
-/* This is still experimental. /bg */
-#define UAODV_BAD_ROUTE
 #define AODV_NUM_RT_ENTRIES 32
 
 /* Helper prototypes that should go somewhere. */
@@ -50,7 +48,7 @@ void msp430_cpu_init(void);	/* Rename to cpu_init() later! */
 #define UIP_CONF_RECEIVE_WINDOW  (UIP_CONF_BUFFER_SIZE - 40)
 #define UIP_CONF_MAX_CONNECTIONS 4
 #define UIP_CONF_MAX_LISTENPORTS 8
-#define UIP_CONF_UDP_CONNS       6
+#define UIP_CONF_UDP_CONNS       12
 #define UIP_CONF_FWCACHE_SIZE    30
 #define UIP_CONF_BROADCAST       1
 #define UIP_ARCH_IPCHKSUM        1
