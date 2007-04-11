6814a72623e30b59b4fc6ef7d2e6b9728b2a30ce
oliverschmidt <oliverschmidt>
2007-04-11 00:26:27 +0000

    Made endianess configuration actually work.

--- platform/cooja/contiki-conf.h
+++ platform/cooja/contiki-conf.h
@@ -247,7 +247,7 @@ architecture specific files to work). */
 #define UIP_CONF_MAX_LISTENPORTS 4
 #define UIP_CONF_BUFFER_SIZE     200
 
-#define UIP_CONF_BYTE_ORDER      LITTLE_ENDIAN
+#define UIP_CONF_BYTE_ORDER      UIP_LITTLE_ENDIAN
 
 #define UIP_CONF_BROADCAST	 1
 
