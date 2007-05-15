8b2464ef9bead7e838aa93d5ef87fa6fdacddc29
adamdunkels <adamdunkels>
2007-05-15 07:51:57 +0000

    A common structure for radio drivers. Used by MAC protocols.

--- core/dev/radio.h
+++ core/dev/radio.h
@@ -55,6 +55,26 @@
 #ifndef __RADIO_H__
 #define __RADIO_H__
 
+/**
+ * The structure of a device driver for a radio in Contiki.
+ */
+struct radio_driver {
+  /** Send a packet  */
+  int (* send)(const u8_t *buf, u16_t len);
+
+  /** Read a received packet. */
+  u16_t (* read)(u8_t *buf, u16_t bufsize);
+
+  /** Set a function to be called when a packet has been received. */
+  void (* set_receive_function)(void (*f)(const struct radio_driver *d));
+
+  /** Turn the radio on. */
+  int (* on)(void);
+
+  /** Turn the radio off. */
+  int (* off)(void);
+};
+
 
 /**
  * \brief      Turn radio on.
