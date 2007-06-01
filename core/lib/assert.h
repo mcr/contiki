ac5bb599e5ab06c2743e771beab50bef346a0101
bg- <bg->
2007-06-01 15:36:00 +0000

    * Add compile time assert CTASSERT.

--- core/lib/assert.h
+++ core/lib/assert.h
@@ -39,5 +39,10 @@
 void _xassert(const char *, int);
 #endif
 
+#ifndef CTASSERT                /* Allow lint to override */
+#define CTASSERT(x)             _CTASSERT(x, __LINE__)
+#define _CTASSERT(x, y)         __CTASSERT(x, y)
+#define __CTASSERT(x, y)        typedef char __assert ## y[(x) ? 1 : -1]
+#endif
 
 #endif /* ASSERT_H */
