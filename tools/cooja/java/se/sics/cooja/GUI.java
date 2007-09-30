1129041ad81600f2fef4ec838d6e1a0d96288a03
fros4943 <fros4943>
2007-09-30 12:02:33 +0000

    cooja mac os x support

--- tools/cooja/java/se/sics/cooja/GUI.java
+++ tools/cooja/java/se/sics/cooja/GUI.java
@@ -74,6 +74,11 @@ public class GUI {
   public static final String EXTERNAL_TOOLS_WIN32_SETTINGS_FILENAME = "/external_tools_win32.config";
 
   /**
+   * External tools default Mac OS X settings filename.
+   */
+  public static final String EXTERNAL_TOOLS_MACOSX_SETTINGS_FILENAME = "/external_tools_macosx.config";
+
+  /**
    * External tools default Linux/Unix settings filename.
    */
   public static final String EXTERNAL_TOOLS_LINUX_SETTINGS_FILENAME = "/external_tools_linux.config";
@@ -2411,10 +2416,16 @@ public class GUI {
    * Load external tools settings from default file.
    */
   public static void loadExternalToolsDefaultSettings() {
+    String osName = System.getProperty("os.name").toLowerCase();
+
     String filename = GUI.EXTERNAL_TOOLS_LINUX_SETTINGS_FILENAME;
-    if (System.getProperty("os.name").startsWith("Win")) {
+    if (osName.startsWith("win")) {
       filename = GUI.EXTERNAL_TOOLS_WIN32_SETTINGS_FILENAME;
+    } else if (osName.startsWith("mac os x")) {
+      filename = GUI.EXTERNAL_TOOLS_MACOSX_SETTINGS_FILENAME;
     }
+
+    logger.info("Loading external tools user settings from: " + filename);
 
     try {
       InputStream in = GUI.class.getResourceAsStream(filename);
