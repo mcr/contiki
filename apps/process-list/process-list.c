580142c519a7a2058a0e80550b828ade956a0489
matsutsuka <matsutsuka>
2007-09-30 02:32:23 +0000

    Removed the last change so that we can assume all global variables are zero-initialized.

--- apps/process-list/process-list.c
+++ apps/process-list/process-list.c
@@ -53,7 +53,7 @@ static struct ctk_label processnamelabels[MAX_PROCESSLABELS];
 
 static struct ctk_label killlabel =
   {CTK_LABEL(0, PROCESSLIST_HEIGHT - 2, 12, 1, "Kill process")};
-static char killprocnum[3] = "";
+static char killprocnum[3];
 static struct ctk_textentry killtextentry =
   {CTK_TEXTENTRY(13, PROCESSLIST_HEIGHT - 2, 2, 1, killprocnum, 2)};
 static struct ctk_button killbutton =
