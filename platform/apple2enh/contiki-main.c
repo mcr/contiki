1a23b56127f7b96c1c706594cadb3652710df728
oliverschmidt <oliverschmidt>
2007-11-25 22:21:19 +0000

    Switched to lowercase file name and do consistently "first-log then operate".

--- platform/apple2enh/contiki-main.c
+++ platform/apple2enh/contiki-main.c
@@ -73,11 +73,11 @@ main(void)
 
 #if 1
   {
-    static struct ethernet_config config = {0xC0B0, "CS8900A.ETH"};
+    static struct ethernet_config config = {0xC0B0, "cs8900a.eth"};
     uip_ipaddr_t addr;
 
-    process_start((struct process *)&ethernet_process, (char *)&config);
     printf("Eth. Driver: %s at $%X\n", config.name, config.addr);
+    process_start((struct process *)&ethernet_process, (char *)&config);
 
     uip_ipaddr(&addr, 192,168,0,128);
     printf("IP Address:  %d.%d.%d.%d\n", uip_ipaddr_to_quad(&addr));
