e87d3edd533e3434a8da5fb0b3db5d6161d2cb36
oliverschmidt <oliverschmidt>
2007-11-27 20:52:52 +0000

    Make use of global error_exit().

--- cpu/6502/net/ethernet.c
+++ cpu/6502/net/ethernet.c
@@ -37,14 +37,14 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
 #include <modload.h>
 
 #include "contiki-net.h"
-#include "ethernet-drv.h"
+#include "lib/error.h"
+#include "net/ethernet-drv.h"
 
-#include "ethernet.h"
+#include "net/ethernet.h"
 
 struct {
   char                signature[4];
@@ -57,14 +57,6 @@ struct {
   void                (* exit)(void);
 } *module;
 
-/*---------------------------------------------------------------------------*/
-static void
-error_exit(void)
-{
-  fprintf(stderr, "Press any key to continue ...\n");
-  getchar();
-  exit(EXIT_FAILURE);
-}
 /*---------------------------------------------------------------------------*/
 void
 ethernet_init(struct ethernet_config *config)
