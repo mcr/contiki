7c617a420af4603130972e34f910c3bf7155ad85
adamdunkels <adamdunkels>
2007-11-28 12:53:07 +0000

    Added #ifdef around label (yes, ugly) to avoid compiler warning with UIP_UDP == 0

--- core/net/uip.c
+++ core/net/uip.c
@@ -1827,8 +1827,10 @@ uip_process(u8_t flag)
   /* Calculate TCP checksum. */
   BUF->tcpchksum = 0;
   BUF->tcpchksum = ~(uip_tcpchksum());
-  
+
+#if UIP_UDP
  ip_send_nolen:
+#endif /* UIP_UDP */
 
 #if UIP_CONF_IPV6
   BUF->vtc = 0x60;
