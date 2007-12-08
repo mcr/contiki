3bd8bdff26cfc7005693a41168e9e1c201f4ea50
oliverschmidt <oliverschmidt>
2007-12-08 23:04:34 +0000

    Allow to actually configure active open support via  UIP_CONF_ACTIVE_OPEN. Is there any reason I don't understand why this wasn't allowed? At least it seems to work...

--- core/net/uipopt.h
+++ core/net/uipopt.h
@@ -240,7 +240,11 @@
  *
  * \hideinitializer
  */
+#ifndef UIP_CONF_ACTIVE_OPEN
 #define UIP_ACTIVE_OPEN 1
+#else /* UIP_CONF_ACTIVE_OPEN */
+#define UIP_ACTIVE_OPEN UIP_CONF_ACTIVE_OPEN
+#endif /* UIP_CONF_ACTIVE_OPEN */
 
 /**
  * The maximum number of simultaneously open TCP connections.
