b957d417d3a20ad48b19721048067ee300bd9a69
nifi <nifi>
2007-12-17 12:17:23 +0000

    changed the way that rime address is set

--- platform/esb/contiki-esb-default-init-net.c
+++ platform/esb/contiki-esb-default-init-net.c
@@ -45,8 +45,7 @@ init_net(void)
 
   tr1001_init();
   rime_init(nullmac_init(&tr1001_driver));
-  rimeaddr.u8[0] = node_id >> 8;
-  rimeaddr.u8[1] = node_id & 0xff;
+  rimeaddr.u16[0] = node_id;
   rimeaddr_set_node_addr(&rimeaddr);
 
   rs232_set_input(serial_input_byte);
