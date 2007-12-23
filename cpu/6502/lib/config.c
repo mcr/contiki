c35865055e53931db0f85cee830da1733ee9d2ca
oliverschmidt <oliverschmidt>
2007-12-23 12:33:57 +0000

    Made use of the new high-level config macros.

--- cpu/6502/lib/config.c
+++ cpu/6502/lib/config.c
@@ -89,7 +89,7 @@ config_read(char *filename)
   log_message("IP Address:  ",  ipaddrtoa(&config.hostaddr, uip_buf));
   log_message("Subnet Mask: ",  ipaddrtoa(&config.netmask, uip_buf));
   log_message("Def. Router: ",  ipaddrtoa(&config.draddr, uip_buf));
-//log_message("DNS Server:  ",  ipaddrtoa(&config.resolvaddr, uip_buf));
+  log_message("DNS Server:  ",  ipaddrtoa(&config.resolvaddr, uip_buf));
 
   log_message("Eth. Driver: ",  config.ethernetcfg.name);
   log_message("Driver Port: $", utoa(config.ethernetcfg.addr, uip_buf, 16));
@@ -97,7 +97,9 @@ config_read(char *filename)
   uip_sethostaddr(&config.hostaddr);
   uip_setnetmask(&config.netmask);
   uip_setdraddr(&config.draddr);
-//resolv_conf(&config.resolvaddr);
+#if WITH_DNS
+  resolv_conf(&config.resolvaddr);
+#endif /* WITH_DNS */
 
   return &config.ethernetcfg;
 }
