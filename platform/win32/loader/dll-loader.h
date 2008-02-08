9e427359c0c050029ab0eb41547b226b0d24ed06
oliverschmidt <oliverschmidt>
2008-02-08 12:14:52 +0000

    Removed superfluous semicolon.

--- platform/win32/loader/dll-loader.h
+++ platform/win32/loader/dll-loader.h
@@ -35,7 +35,7 @@
 #ifndef __DLL_LOADER_H__
 #define __DLL_LOADER_H__
 
-#define LOADER_LOAD(name, arg) dll_loader_load(name, arg);
+#define LOADER_LOAD(name, arg) dll_loader_load(name, arg)
 #define LOADER_UNLOAD()        dll_loader_unload((void *)autostart_processes)
 #define LOADER_LOAD_DSC(name)  dll_loader_load_dsc(name)
 #define LOADER_UNLOAD_DSC(dsc) dll_loader_unload_dsc(dsc)
