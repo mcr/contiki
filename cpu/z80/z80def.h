42e6e1fd88fde57dd2842b9f0b961faa1b661340
matsutsuka <matsutsuka>
2008-07-02 07:13:22 +0000

    Modified some glitches in startup code, and introduced additional typedefs required in the current version.

--- cpu/z80/z80def.h
+++ cpu/z80/z80def.h
@@ -50,7 +50,9 @@
 
 
 /* Generic types. */
+typedef   signed char    int8_t;
 typedef unsigned char   uint8_t;
+typedef   signed short  int16_t;
 typedef unsigned short uint16_t;
 typedef unsigned long  uint32_t;
 typedef unsigned char   u8_t;      /* 8 bit type */
