a18a2c496f4a67e112bbaa8942509f3cc69b4e4e
nvt-se <nvt-se>
2008-07-04 02:58:58 +0000

    energest.h moved to sys/

--- platform/esb/energest-arch.c
+++ platform/esb/energest-arch.c
@@ -38,9 +38,8 @@
  *         Adam Dunkels <adam@sics.se>
  */
 
-#include "lib/energest.h"
-
 #include "sys/clock.h"
+#include "sys/energest.h"
 #include "sys/rtimer.h"
 
 #define DEC2FIX(h,d) ((h << 6) + (unsigned long)(0.64 * d))
