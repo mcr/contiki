db165dac47c564d40e7c25eb33c6ceaabd253125
adamdunkels <adamdunkels>
2008-07-07 23:45:35 +0000

    Removed netfile command, added net debug commands

--- examples/sky-shell/sky-shell.c
+++ examples/sky-shell/sky-shell.c
@@ -101,10 +101,11 @@ PROCESS_THREAD(test_shell_process, ev, data)
   shell_coffee_init();
   /*  shell_exec_init();*/
   shell_file_init();
-  shell_netfile_init();
+  /*  shell_netfile_init();*/
   shell_ps_init();
   shell_reboot_init();
   shell_rime_init();
+  shell_rime_debug_init();
   shell_rime_netcmd_init();
   shell_rime_ping_init();
   shell_rime_sniff_init();
