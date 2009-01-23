a64e7155e9e46bf21537ba3ba87956c432816351
fros4943 <fros4943>
2009-01-23 17:08:17 +0000

    removed compiler warning by returning 0 at undefined value

--- platform/sky/dev/acc-sensor.c
+++ platform/sky/dev/acc-sensor.c
@@ -70,7 +70,7 @@ activate(void)
   P2DIR |= 0x48;
   P2OUT |= 0x48;
 
-  
+
   /* stop converting immediately */
   ADC12CTL0 &= ~ENC;
   ADC12CTL1 &= ~CONSEQ_3;
@@ -80,7 +80,7 @@ activate(void)
   ADC12MCTL2 = (INCH_4 + SREF_1);
   ADC12MCTL3 = (INCH_5 + SREF_1);
   ADC12MCTL4 = (INCH_6 + SREF_1);
-  
+
   ADC12CTL1 |= CONSEQ_3;
   ADC12CTL0 |= ENC | ADC12SC;
 
@@ -111,6 +111,7 @@ value(int type)
   case 2:
     return ADC12MEM4;
   }
+  return 0;
 }
 /*---------------------------------------------------------------------------*/
 static int
