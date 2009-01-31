7fdcd3a8a63980da2e877ee3f4958eb35cff0bd4
joxe <joxe>
2009-01-31 12:46:57 +0000

    added handling function for checkin if uart is transmitting or receiving - patch by Klaus Stengel

--- cpu/msp430/dev/uart1.h
+++ cpu/msp430/dev/uart1.h
@@ -48,5 +48,6 @@
 void uart1_set_input(int (*input)(unsigned char c));
 void uart1_writeb(unsigned char c);
 void uart1_init(unsigned long ubr);
+uint8_t uart1_active(void);
 
 #endif /* __UART1_H__ */
