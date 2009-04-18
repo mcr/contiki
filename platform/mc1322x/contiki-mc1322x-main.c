d7bd4c34f69128be58b0439faec4714083a6cac9
Mariano Alvira <mar@devl.org>
2009-04-18 15:16:59 -0400

    hardcode mc1322x-main to blink the red led.
    this proves the build system is sane.

--- platform/mc1322x/contiki-mc1322x-main.c
+++ platform/mc1322x/contiki-mc1322x-main.c
@@ -44,10 +44,36 @@ init_lowlevel(void)
 {
 }
 
+#define MBAR_GPIO       0x80000000
+#define GPIO_PAD_DIR0   0x80000000
+#define GPIO_DATA0      0x80000008
+#define UART1_DATA      0x80005008
+#define DELAY 400000
+
+
 
 int
 main(void)
 {
+
+  *(volatile uint32_t *)GPIO_PAD_DIR0 = 0x00000100;
+  
+  volatile uint32_t i;
+  
+  while(1) {
+	  
+	  *(volatile uint32_t *)GPIO_DATA0 = 0x00000100;
+	  
+	  for(i=0; i<DELAY; i++) { continue; }
+	  
+	  *(volatile uint32_t *)GPIO_DATA0 = 0x00000000;
+	  
+	  for(i=0; i<DELAY; i++) { continue; }
+	  
+  };
+
+
+
   /* Initialize hardware */
   init_lowlevel();
 
