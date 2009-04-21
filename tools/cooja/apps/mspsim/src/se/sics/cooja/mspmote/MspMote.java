16495037e6724c5928b18fa6a19038de4efebc8c
fros4943 <fros4943>
2009-04-21 18:22:41 +0000

    increasing loop symbols for higher clock speed

--- tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/MspMote.java
+++ tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/MspMote.java
@@ -67,7 +67,8 @@ public abstract class MspMote implements Mote {
   private static Logger logger = Logger.getLogger(MspMote.class);
 
   /* 2.4576 MHz according to Contiki's speed sync loop*/
-  public static long NR_CYCLES_PER_MSEC = 2458;
+  //public static long NR_CYCLES_PER_MSEC = 2458xx;
+  public static long NR_CYCLES_PER_MSEC = 3900;
 
   /* Cycle counter */
   public long cycleCounter = 0;
