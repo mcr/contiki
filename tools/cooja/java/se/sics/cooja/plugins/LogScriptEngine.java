1c59eb65a3190c0caec61e83c1da8ef82ab86ffb
fros4943 <fros4943>
2009-04-23 10:41:35 +0000

    release semaphores before stopping simulation (may deadlock)

--- tools/cooja/java/se/sics/cooja/plugins/LogScriptEngine.java
+++ tools/cooja/java/se/sics/cooja/plugins/LogScriptEngine.java
@@ -338,7 +338,6 @@ public class LogScriptEngine {
         /*logger.info("test script thread starts");*/
         try {
           ((Invocable)engine).getInterface(Runnable.class).run();
-
         } catch (RuntimeException e) {
           Throwable throwable = e;
           while (throwable.getCause() != null) {
@@ -347,7 +346,7 @@ public class LogScriptEngine {
 
           if (throwable.getMessage() != null &&
               throwable.getMessage().contains("test script killed") ) {
-            /*logger.info("test script thread terminated by exception");*/
+            logger.info("Test script finished");
           } else {
             if (!GUI.isVisualized()) {
               logger.fatal("Test script error, terminating Cooja.");
@@ -356,8 +355,8 @@ public class LogScriptEngine {
             }
 
             logger.fatal("Script error:", e);
-            gui.getSimulation().stopSimulation();
             deactivateScript();
+            gui.getSimulation().stopSimulation();
           }
         }
         /*logger.info("test script thread exits");*/
