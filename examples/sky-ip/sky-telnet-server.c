60a697e5e6594d9d3930bfa008da7b139d1e1d04
fros4943 <fros4943>
2009-06-26 09:51:18 +0000

    disabled rime commands to decrease rom usage

--- examples/sky-ip/sky-telnet-server.c
+++ examples/sky-ip/sky-telnet-server.c
@@ -80,21 +80,21 @@ PROCESS_THREAD(sky_telnetd_process, ev, data)
       shell_netfile_init();*/
   shell_ps_init();
   shell_reboot_init();
-  shell_rime_init();
-  shell_rime_ping_init();
-  shell_rime_netcmd_init();
-  shell_rime_sniff_init();
+  /*shell_rime_init();*/
+  /*shell_rime_ping_init();*/
+  /*shell_rime_netcmd_init();*/
+  /*shell_rime_sniff_init();*/
   shell_sky_init();
   shell_text_init();
   shell_time_init();
 
   shell_register_command(&id_command);
-  
+
   while(1) {
     PROCESS_WAIT_EVENT();
   }
-  
+
   PROCESS_END();
-    
+
 }
 /*---------------------------------------------------------------------------*/
