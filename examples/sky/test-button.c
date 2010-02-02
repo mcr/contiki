f5228ed47af2984ecae9e6192f8fe0ad2a550c89
joxe <joxe>
2010-02-02 21:17:32 +0000

    added activation of button sensor and activation and deactivation / toggle of light sensor

--- examples/sky/test-button.c
+++ examples/sky/test-button.c
@@ -38,6 +38,7 @@
  *         Adam Dunkels <adam@sics.se>
  */
 
+#include <stdio.h>
 #include "contiki.h"
 #include "dev/button-sensor.h"
 #include "dev/light-sensor.h"
@@ -48,17 +49,25 @@
 PROCESS(test_button_process, "Test button");
 AUTOSTART_PROCESSES(&test_button_process);
 /*---------------------------------------------------------------------------*/
+static uint8_t active;
 PROCESS_THREAD(test_button_process, ev, data)
 {
   PROCESS_BEGIN();
-
-  SENSORS_ACTIVATE(light_sensor);
-  
+  SENSORS_ACTIVATE(button_sensor);
   while(1) {
     PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event &&
 			     data == &button_sensor);
     leds_toggle(LEDS_ALL);
-    printf("Light: %d\n", light_sensor.value(0));
+    if (!active) {
+      /* activate light sensor */
+      SENSORS_ACTIVATE(light_sensor);
+      printf("Light: %d\n", light_sensor.value(0));
+    } else {
+      /* deactivate light sensor */
+      printf("Light: %d\n", light_sensor.value(0));
+      SENSORS_DEACTIVATE(light_sensor);
+    }
+    leds_toggle(LEDS_ALL);
   }
   
   PROCESS_END();
