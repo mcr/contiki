b363e4681e63b50ff887d35dc80ee764bb6afb71
adamdunkels <adamdunkels>
2010-02-15 17:47:01 +0000

    Added power & Rime debug commands

--- examples/sky-shell/sky-shell.c
+++ examples/sky-shell/sky-shell.c
@@ -248,9 +248,10 @@ PROCESS_THREAD(sky_shell_process, ev, data)
   shell_rime_init();
   shell_rime_netcmd_init();
   shell_rime_ping_init();
-/*  shell_rime_debug_init(); */
-/*  shell_rime_sniff_init();*/
+  shell_rime_debug_init();
+  shell_rime_sniff_init();
   shell_sky_init();
+  shell_power_init();
   shell_base64_init();
   shell_text_init();
   shell_time_init();
