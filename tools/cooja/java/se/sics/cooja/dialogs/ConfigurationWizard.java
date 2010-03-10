ab677d0d8def1bf932d1042d299a919ea01e6bbe
fros4943 <fros4943>
2010-03-10 07:49:25 +0000

    removed contiki motes' communication stack configuration; the network stack instead depends on compiler flags/contiki-conf.h

--- tools/cooja/java/se/sics/cooja/dialogs/ConfigurationWizard.java
+++ tools/cooja/java/se/sics/cooja/dialogs/ConfigurationWizard.java
@@ -69,7 +69,6 @@ import se.sics.cooja.GUI;
 import se.sics.cooja.SectionMoteMemory;
 import se.sics.cooja.MoteType.MoteTypeCreationException;
 import se.sics.cooja.contikimote.ContikiMoteType;
-import se.sics.cooja.contikimote.ContikiMoteType.CommunicationStack;
 
 /* TODO Test common section */
 
@@ -602,8 +601,7 @@ public class ConfigurationWizard extends JDialog {
           new File(cLibraryName + ".c"),
           new File(cLibraryName + ContikiMoteType.mapSuffix),
           new File(cLibraryName + ContikiMoteType.librarySuffix),
-          new File(cLibraryName + ContikiMoteType.dependSuffix),
-          CommunicationStack.RIME
+          new File(cLibraryName + ContikiMoteType.dependSuffix)
       );
     } catch (Exception e) {
       testOutput.addMessage("### Error: Compiler environment failed", MessageList.ERROR);
