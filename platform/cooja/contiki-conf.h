818fb4dd5135ca3e66d14aeae0da754e744fd7b0
fros4943 <fros4943>
2010-03-10 12:18:29 +0000

    added ipv4 and ipv6 default configuration

--- platform/cooja/contiki-conf.h
+++ platform/cooja/contiki-conf.h
@@ -40,22 +40,52 @@
 
 #define COOJA 1
 
+#if WITH_UIP
+#if WITH_UIP6
+#error WITH_UIP && WITH_IP6: Bad configuration
+#endif /* WITH_UIP6 */
+#endif /* WITH_UIP */
 
 #ifdef NETSTACK_CONF_H
 
+/* These header overrides the below default configuration */
 #define NETSTACK__QUOTEME(s) NETSTACK_QUOTEME(s)
 #define NETSTACK_QUOTEME(s) #s
 #include NETSTACK__QUOTEME(NETSTACK_CONF_H)
 
 #else /* NETSTACK_CONF_H */
 
-/* Default network config: Rime */
-/* These settings may be overriden by NETSTACK_CONF_H below */
+/* Default network config */
+#if WITH_UIP6
+
+/* Network setup for IPv6 */
+#define NETSTACK_CONF_NETWORK uip_driver
+#define NETSTACK_CONF_MAC nullmac_driver
+#define NETSTACK_CONF_RDC nullrdc_driver
+#define NETSTACK_CONF_RADIO cooja_radio_driver
+#define UIP_CONF_IPV6 1
+
+#else /* WITH_UIP6 */
+
+#if WITH_UIP
+
+/* Network setup for IPv4 */
+#define NETSTACK_CONF_NETWORK uip_driver
+#define NETSTACK_CONF_MAC nullmac_driver
+#define NETSTACK_CONF_RDC nullrdc_driver
+#define NETSTACK_CONF_RADIO cooja_radio_driver
+
+#else /* WITH_UIP */
+
+/* Network setup for Rime */
 #define NETSTACK_CONF_NETWORK rime_driver
 #define NETSTACK_CONF_MAC nullmac_driver
 #define NETSTACK_CONF_RDC nullrdc_driver
 #define NETSTACK_CONF_RADIO cooja_radio_driver
 /*#define NETSTACK_CONF_FRAMER framer_nullmac*/
+
+#endif /* WITH_UIP */
+#endif /* WITH_UIP6 */
 
 #endif /* NETSTACK_CONF_H */
 
