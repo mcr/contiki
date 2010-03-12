7aeeca26a1054939a7cf41278685cb472b91c5c1
dak664 <dak664>
2010-03-12 15:21:14 +0000

    Remove compiler warning

--- core/net/rime/collect.c
+++ core/net/rime/collect.c
@@ -628,7 +628,7 @@ collect_open(struct collect_conn *tc, uint16_t channels,
   neighbor_discovery_open(&tc->neighbor_discovery_conn, channels,
 			  CLOCK_SECOND * 8,
 			  CLOCK_SECOND * 32,
-			  (CLOCK_SECOND * 600UL),
+			  (unsigned)(CLOCK_SECOND * 600UL),
 			  &neighbor_discovery_callbacks);
   neighbor_discovery_start(&tc->neighbor_discovery_conn, tc->rtmetric);
 #else /* !COLLECT_ANNOUNCEMENTS */
