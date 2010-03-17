dda06341a5d0cc2e7a36e33d25af10756aa88183
Mariano Alvira <mar@devl.org>
2010-03-17 13:44:26 -0400

    libmc1322x should be converted to use stdint.h

--- cpu/mc1322x/lib/include/types.h
+++ cpu/mc1322x/lib/include/types.h
@@ -1,13 +1,6 @@
 #ifndef  _TYPES_H
 #define _TYPES_H
 
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef signed int int16_t;
-typedef unsigned int uint16_t;
-typedef signed long int int32_t;
-typedef unsigned long int uint32_t;
-typedef signed long long int int64_t;
-typedef unsigned long long int uint64_t; 
+#include <stdint.h>
 
 #endif
