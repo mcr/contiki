2fb10a236107f46950338a6bf5178f69fa552bd8
joxe <joxe>
2010-03-19 14:50:07 +0000

    activated the new workaround...

--- cpu/msp430/msp430.c
+++ cpu/msp430/msp430.c
@@ -39,28 +39,28 @@
 #include "net/uip.h"
 
 /* /\*---------------------------------------------------------------------------*\/ */
-/* void * */
-/* w_memcpy(void *out, const void *in, size_t n) */
-/* { */
-/*   uint8_t *src, *dest; */
-/*   src = (uint8_t *) in; */
-/*   dest = (uint8_t *) out; */
-/*   while(n-- > 0) { */
-/*     *dest++ = *src++; */
-/*   } */
-/*   return out; */
-/* } */
-/* /\*---------------------------------------------------------------------------*\/ */
-/* void * */
-/* w_memset(void *out, int value, size_t n) */
-/* { */
-/*   uint8_t *dest; */
-/*   dest = (uint8_t *) out; */
-/*   while(n-- > 0) { */
-/*     *dest++ = value & 0xff; */
-/*   } */
-/*   return out; */
-/* } */
+void *
+w_memcpy(void *out, const void *in, size_t n)
+{
+  uint8_t *src, *dest;
+  src = (uint8_t *) in;
+  dest = (uint8_t *) out;
+  while(n-- > 0) {
+    *dest++ = *src++;
+  }
+  return out;
+}
+/*---------------------------------------------------------------------------*/
+void *
+w_memset(void *out, int value, size_t n)
+{
+  uint8_t *dest;
+  dest = (uint8_t *) out;
+  while(n-- > 0) {
+    *dest++ = value & 0xff;
+  }
+  return out;
+}
 /*---------------------------------------------------------------------------*/
 void
 msp430_init_dco(void)
