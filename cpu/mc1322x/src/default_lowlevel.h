52396d421195469bddd7367853647145f40072b2
Mariano Alvira <mar@devl.org>
2010-03-30 11:12:01 -0400

    fix typo

--- cpu/mc1322x/src/default_lowlevel.h
+++ cpu/mc1322x/src/default_lowlevel.h
@@ -1,7 +1,9 @@
 #ifndef LOWLEVEL_H
 #define LOWLEVEL_H
 
-#define trim_xtal() pack_XTAL_CNTL(CTUNE_4PF, CTUNE, FTUNE, IBIAS);
+#include "types.h"
+
+#define trim_xtal() pack_XTAL_CNTL(CTUNE_4PF, CTUNE, FTUNE, IBIAS)
 void default_vreg_init(void);
 void uart1_init(uint16_t inc, uint16_t mod, uint8_t samp);
 
