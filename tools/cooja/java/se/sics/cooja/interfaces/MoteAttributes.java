2dac028081e9be95d8372648a2fc578ad1984c6c
fros4943 <fros4943>
2010-05-17 11:41:22 +0000

    attribute summary method made public

--- tools/cooja/java/se/sics/cooja/interfaces/MoteAttributes.java
+++ tools/cooja/java/se/sics/cooja/interfaces/MoteAttributes.java
@@ -144,7 +144,7 @@ public class MoteAttributes extends MoteInterface {
       }
   }
   
-  private String getText() {
+  public String getText() {
       StringBuffer sb = new StringBuffer();
       Object[] keys = attributes.keySet().toArray();
       for (int i = 0; i < keys.length; i++) {
