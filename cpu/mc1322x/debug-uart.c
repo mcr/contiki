dadea46b7ac2322113043b80685c9a713bab33a3
Mariano Alvira <mar@devl.org>
2010-06-02 11:29:02 -0400

    fix debug uart

--- cpu/mc1322x/debug-uart.c
+++ cpu/mc1322x/debug-uart.c
@@ -5,8 +5,8 @@ dbg_send_bytes(const unsigned char *s, unsigned int len)
 {
 	unsigned int i=0;
 	while(s && *s!=0) {
+		if( i >= len) { break; }
 		dbg_putchar(*s++); i++;
-		if( i > len) { continue; }
 	}
 	return i;
 }
