5c0ee9a0a4347526733215524b455a2eb395e51b
dak664 <dak664>
2010-09-14 18:55:04 +0000

    Add option to remove process name strings to save RAM

--- core/sys/process.h
+++ core/sys/process.h
@@ -292,22 +292,35 @@ static PT_THREAD(process_thread_##name(struct pt *process_pt,	\
  * This macro declares a process. The process has two names: the
  * variable of the process structure, which is used by the C program,
  * and a human readable string name, which is used when debugging.
+ * A configuration option allows removal of the readable name to save RAM.
  *
  * \param name The variable name of the process structure.
  * \param strname The string representation of the process' name.
  *
  * \hideinitializer
  */
+#if PROCESS_CONF_NO_PROCESS_NAMES
+#define PROCESS(name, strname)				\
+  PROCESS_THREAD(name, ev, data);			\
+  struct process name = { NULL,		        \
+                          process_thread_##name }
+#else
 #define PROCESS(name, strname)				\
   PROCESS_THREAD(name, ev, data);			\
   struct process name = { NULL, strname,		\
                           process_thread_##name }
+#endif
 
 /** @} */
 
 struct process {
   struct process *next;
+#if PROCESS_CONF_NO_PROCESS_NAMES
+#define PROCESS_NAME_STRING(process) ""
+#else
   const char *name;
+#define PROCESS_NAME_STRING(process) (process)->name
+#endif
   PT_THREAD((* thread)(struct pt *, process_event_t, process_data_t));
   struct pt pt;
   unsigned char state, needspoll;
