d1a94f6244a59e5be54b458ff9c9d01c2384d8ce
maralvira <maralvira>
2011-01-17 15:48:39 +0000

    whitespace fixup to sync with libmc1322x

--- cpu/mc1322x/src/default_lowlevel.h
+++ cpu/mc1322x/src/default_lowlevel.h
@@ -39,6 +39,7 @@
 #include "crm.h"
 
 #define trim_xtal() pack_XTAL_CNTL(CTUNE_4PF, CTUNE, FTUNE, IBIAS)
+
 void default_vreg_init(void);
 void uart1_init(uint16_t inc, uint16_t mod, uint8_t samp);
 
